d0a2cd945a20a1c74db113aa26e1edf4de0a2d83
sxndwi <fomchenkos.137@gmail.com>
2024-01-21 16:31:18 +0300

    fix)))

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,6 @@ import { useLocalHost } from "./config";
 export const generateImageUrl = (imageUrl) => {
   const baseURL = useLocalHost
     ? "http://localhost:4444"
-    : "https://fotaku.me";
+    : "https://fotaku.me/api";
   return imageUrl ? `${baseURL}${imageUrl}` : "";
 };
